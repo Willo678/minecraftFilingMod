19d777b8bbaaacc12f651fbb2f641dd4e28a1f05
William Postill <wp22246@essex.ac.uk>
2025-07-09 12:04:52 +0100

    Block now renders correctly, occupying a half vertical block. Item also displays model.

--- src/main/java/net/willo678/filingcabinet/block/FilingCabinetBlock.java
+++ src/main/java/net/willo678/filingcabinet/block/FilingCabinetBlock.java
@@ -1,45 +1,69 @@
 package net.willo678.filingcabinet.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.network.NetworkHooks;
 import net.willo678.filingcabinet.block.entity.FilingCabinetBlockEntity;
 import net.willo678.filingcabinet.block.entity.ModBlockEntities;
 import org.jetbrains.annotations.Nullable;
 
-public class FilingCabinetBlock extends BaseEntityBlock {
+public class FilingCabinetBlock extends HorizontalDirectionalBlock implements EntityBlock {
+
+    private static final VoxelShape AABB_NORTH_HALF = Block.box(0, 0, 8, 16, 16, 16);
+    private static final VoxelShape AABB_SOUTH_HALF = Block.box(0, 0, 0, 16, 16, 8);
+    private static final VoxelShape AABB_WEST_HALF = Block.box(8, 0, 0, 16, 16, 16);
+    private static final VoxelShape AABB_EAST_HALF = Block.box(0, 0, 0, 8, 16, 16);
+
 
 
     public FilingCabinetBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(getStateDefinition().any()
+                .setValue(FACING, Direction.NORTH));
     }
 
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
 
-
-
-
-
-
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
 
     /* BLOCK ENTITY */
 
+    @SuppressWarnings("deprecation")
     @Override
-    public RenderShape getRenderShape(BlockState p_49232_) {
-        return RenderShape.MODEL;
+    public VoxelShape getShape(BlockState state, BlockGetter worldIn, BlockPos pos, CollisionContext context) {
+        return switch (state.getValue(FACING)) {
+            case EAST -> AABB_EAST_HALF;
+            case WEST -> AABB_WEST_HALF;
+            case SOUTH -> AABB_SOUTH_HALF;
+            default -> AABB_NORTH_HALF;
+        };
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public void onRemove(BlockState blockstate, Level level, BlockPos blockPos, BlockState newState, boolean isMoving) {
         if (blockstate.getBlock() != newState.getBlock()) {
@@ -55,7 +79,14 @@ public class FilingCabinetBlock extends BaseEntityBlock {
         super.onRemove(blockstate, level, blockPos, newState, isMoving);
     }
 
+    @SuppressWarnings("deprecation")
+    public boolean triggerEvent(BlockState p_49226_, Level p_49227_, BlockPos p_49228_, int p_49229_, int p_49230_) {
+        super.triggerEvent(p_49226_, p_49227_, p_49228_, p_49229_, p_49230_);
+        BlockEntity blockentity = p_49227_.getBlockEntity(p_49228_);
+        return blockentity == null ? false : blockentity.triggerEvent(p_49229_, p_49230_);
+    }
 
+    @SuppressWarnings("deprecation")
     @Override
     public InteractionResult use(BlockState pBlockState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
         if (!pLevel.isClientSide) {
@@ -74,6 +105,12 @@ public class FilingCabinetBlock extends BaseEntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
         return new FilingCabinetBlockEntity(pPos, pState);
+    }
+
+
+    @javax.annotation.Nullable
+    protected static <E extends BlockEntity, A extends BlockEntity> BlockEntityTicker<A> createTickerHelper(BlockEntityType<A> p_152133_, BlockEntityType<E> p_152134_, BlockEntityTicker<? super E> p_152135_) {
+        return p_152134_ == p_152133_ ? (BlockEntityTicker<A>)p_152135_ : null;
     }
 
     @Nullable
