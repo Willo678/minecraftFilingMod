ebd7254fa922b2c1be589f0b1f79d4f0932ba014
William Postill <wp22246@essex.ac.uk>
2025-07-11 16:14:07 +0100

    Created SingleItemHolder class to be used as datastructure for FilingCabinet

---
+++ src/main/java/net/willo678/filingcabinet/util/SingleItemHolder.java
@@ -0,0 +1,311 @@
+package net.willo678.filingcabinet.util;
+
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+public class SingleItemHolder {
+    private Item itemType;
+    private Map<ItemStack, Integer> items;
+
+    public SingleItemHolder() {
+        itemType = null;
+        items = new HashMap<>();
+    }
+
+    public boolean canContainItem(ItemStack itemStack) {
+        return (itemType==null) || (itemStack.getItem()==itemType);
+    }
+
+    public boolean putItem(ItemStack itemStack) {
+        if (!canContainItem(itemStack)) {return false;}
+        itemType = itemType.asItem();
+        items.merge(getSingleItemstack(itemStack), itemStack.getCount(), Integer::sum);
+        return true;
+    }
+
+    public Integer getItem(ItemStack itemStack) {
+        ItemStack toReturn = getSingleItemstack(itemStack);
+        return items.getOrDefault(toReturn, 0);
+    }
+
+    public ItemStack popItem(ItemStack itemStack) {
+        if (contains(itemStack)) {
+            popItem(itemStack, getItem(itemStack));
+        }
+        return null;
+    }
+
+    public ItemStack popItem(ItemStack itemStack, int maxQuantity) {
+        int actualQuantity = Math.min(maxQuantity, items.getOrDefault(itemStack, 0));
+        if (actualQuantity==0) {return null;}
+        else {
+            ItemStack toReturn = getSingleItemstack(itemStack);
+            items.merge(toReturn, -actualQuantity, Integer::sum);
+
+            if (items.get(toReturn) <= 0) {
+                items.remove(toReturn);
+                if (isEmpty()) {itemType = null;}
+            }
+
+            toReturn.setCount(actualQuantity);
+            return toReturn;
+        }
+    }
+
+    public NonNullList<ItemStack> toNonNullList() {
+        NonNullList<ItemStack> itemList = NonNullList.createWithCapacity(this.items.size());
+
+        itemList.clear();
+        for (Map.Entry<ItemStack, Integer> entry : items.entrySet()) {
+            if (canContainItem(entry.getKey())) {
+                itemList.add(makeItemStackOfCount(entry.getKey(), entry.getValue()));
+            } else {
+                itemList.add(ItemStack.EMPTY);
+            }
+        }
+
+        return itemList;
+    }
+
+    public void fromNonNullList(NonNullList<ItemStack> list) {
+        clear();
+
+        for (ItemStack itemStack : list) {
+            if (canContainItem(itemStack)) {
+                putItem(itemStack);
+            }
+        }
+    }
+
+
+    private ItemStack getSingleItemstack(ItemStack itemStack) {
+        ItemStack toReturn = itemStack.copy();
+        toReturn.setCount(1);
+        return toReturn;
+    }
+
+    private ItemStack makeItemStackOfCount(ItemStack itemStack, int count) {
+        ItemStack copy = itemStack.copy();
+        copy.setCount(count);
+        return copy;
+    }
+
+
+
+
+    /*
+    Collections Framework methods
+     */
+
+
+    /**
+     * Returns the number of elements in this collection.  If this collection
+     * contains more than {@code Integer.MAX_VALUE} elements, returns
+     * {@code Integer.MAX_VALUE}.
+     *
+     * @return the number of elements in this collection
+     */
+    public int size() {
+        return items.size();
+    }
+
+    /**
+     * Returns {@code true} if this collection contains no elements.
+     *
+     * @return {@code true} if this collection contains no elements
+     */
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+
+    /**
+     * Returns {@code true} if this collection contains the specified element.
+     * More formally, returns {@code true} if and only if this collection
+     * contains at least one element {@code e} such that
+     * {@code Objects.equals(o, e)}.
+     *
+     * @param o element whose presence in this collection is to be tested
+     * @return {@code true} if this collection contains the specified
+     * element
+     * @throws ClassCastException   if the type of the specified element
+     *                              is incompatible with this collection
+     *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
+     * @throws NullPointerException if the specified element is null and this
+     *                              collection does not permit null elements
+     *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
+     */
+    public boolean contains(Object o) {
+        if (o instanceof ItemStack itemStack) {return items.containsKey(getSingleItemstack(itemStack));}
+        else {return false;}
+    }
+
+
+    /**
+     * Removes a single instance of the specified element from this
+     * collection, if it is present (optional operation).  More formally,
+     * removes an element {@code e} such that
+     * {@code Objects.equals(o, e)}, if
+     * this collection contains one or more such elements.  Returns
+     * {@code true} if this collection contained the specified element (or
+     * equivalently, if this collection changed as a result of the call).
+     *
+     * @param o element to be removed from this collection, if present
+     * @return {@code true} if an element was removed as a result of this call
+     * @throws ClassCastException            if the type of the specified element
+     *                                       is incompatible with this collection
+     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
+     * @throws NullPointerException          if the specified element is null and this
+     *                                       collection does not permit null elements
+     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
+     * @throws UnsupportedOperationException if the {@code remove} operation
+     *                                       is not supported by this collection
+     */
+    public boolean remove(Object o) {
+        if (o instanceof ItemStack itemStack) {
+            return (items.remove(getSingleItemstack(itemStack))!=null);
+        }
+        return false;
+    }
+
+    /**
+     * Returns {@code true} if this collection contains all the elements
+     * in the specified collection.
+     *
+     * @param c collection to be checked for containment in this collection
+     * @return {@code true} if this collection contains all the elements
+     * in the specified collection
+     * @throws ClassCastException   if the types of one or more elements
+     *                              in the specified collection are incompatible with this
+     *                              collection
+     *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
+     * @throws NullPointerException if the specified collection contains one
+     *                              or more null elements and this collection does not permit null
+     *                              elements
+     *                              (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>),
+     *                              or if the specified collection is null.
+     * @see #contains(Object)
+     */
+    public boolean containsAll(@NotNull Collection<?> c) {
+        for (Object o : c) {
+            if (!contains(o)) {return false;}
+        }
+        return true;
+    }
+
+    /**
+     * Adds all the elements in the specified collection to this collection
+     * (optional operation).  The behavior of this operation is undefined if
+     * the specified collection is modified while the operation is in progress.
+     * (This implies that the behavior of this call is undefined if the
+     * specified collection is this collection, and this collection is
+     * nonempty.)
+     *
+     * @param c collection containing elements to be added to this collection
+     * @return {@code true} if this collection changed as a result of the call
+     * @throws UnsupportedOperationException if the {@code addAll} operation
+     *                                       is not supported by this collection
+     * @throws ClassCastException            if the class of an element of the specified
+     *                                       collection prevents it from being added to this collection
+     * @throws NullPointerException          if the specified collection contains a
+     *                                       null element and this collection does not permit null elements,
+     *                                       or if the specified collection is null
+     * @throws IllegalArgumentException      if some property of an element of the
+     *                                       specified collection prevents it from being added to this
+     *                                       collection
+     * @throws IllegalStateException         if not all the elements can be added at
+     *                                       this time due to insertion restrictions
+     * @see #putItem(ItemStack)
+     */
+    public boolean addAll(@NotNull Collection<? extends ItemStack> c) {
+        for (ItemStack itemStack : c) {
+            if (putItem(itemStack)) {
+                continue;
+            }
+
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Removes all of this collection's elements that are also contained in the
+     * specified collection (optional operation).  After this call returns,
+     * this collection will contain no elements in common with the specified
+     * collection.
+     *
+     * @param c collection containing elements to be removed from this collection
+     * @return {@code true} if this collection changed as a result of the
+     * call
+     * @throws UnsupportedOperationException if the {@code removeAll} method
+     *                                       is not supported by this collection
+     * @throws ClassCastException            if the types of one or more elements
+     *                                       in this collection are incompatible with the specified
+     *                                       collection
+     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
+     * @throws NullPointerException          if this collection contains one or more
+     *                                       null elements and the specified collection does not support
+     *                                       null elements
+     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>),
+     *                                       or if the specified collection is null
+     * @see #remove(Object)
+     * @see #contains(Object)
+     */
+    public boolean removeAll(@NotNull Collection<?> c) {
+        boolean flag = false;
+        for (Object o : c) {
+            if (remove(o)) {flag = true;}
+        }
+        return flag;
+    }
+
+    /**
+     * Retains only the elements in this collection that are contained in the
+     * specified collection (optional operation).  In other words, removes from
+     * this collection all of its elements that are not contained in the
+     * specified collection.
+     *
+     * @param c collection containing elements to be retained in this collection
+     * @return {@code true} if this collection changed as a result of the call
+     * @throws UnsupportedOperationException if the {@code retainAll} operation
+     *                                       is not supported by this collection
+     * @throws ClassCastException            if the types of one or more elements
+     *                                       in this collection are incompatible with the specified
+     *                                       collection
+     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>)
+     * @throws NullPointerException          if this collection contains one or more
+     *                                       null elements and the specified collection does not permit null
+     *                                       elements
+     *                                       (<a href="{@docRoot}/java.base/java/util/Collection.html#optional-restrictions">optional</a>),
+     *                                       or if the specified collection is null
+     * @see #remove(Object)
+     * @see #contains(Object)
+     */
+    public boolean retainAll(@NotNull Collection<?> c) {
+        boolean flag = false;
+        for (ItemStack key : items.keySet()) {
+            if (!c.contains(key)) {
+                items.remove(key);
+                flag = true;
+            }
+        }
+        return flag;
+    }
+
+    /**
+     * Removes all the elements from this collection (optional operation).
+     * The collection will be empty after this method returns.
+     *
+     * @throws UnsupportedOperationException if the {@code clear} operation
+     *                                       is not supported by this collection
+     */
+    public void clear() {
+        this.items = new HashMap<>();
+        this.itemType = null;
+    }
+
+
+}
